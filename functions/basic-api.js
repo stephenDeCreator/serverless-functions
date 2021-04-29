99d1b80f7e64046a84fa6acaaef16698a30fc839
Stephen Yeboah <stephenyeb52@gmail.com>
2021-04-29 17:48:52 +0000

    cors error fixed

--- functions/basic-api.js
+++ functions/basic-api.js
@@ -2,6 +2,9 @@ const items = require('../assets/data');
 
 exports.handler = async (event, context) => {
 	return {
+		headers: {
+			'Acess-Control-Allow-Origin': '*'
+		},
 		statusCode: 200,
 		body: JSON.stringify(items)
 	};
