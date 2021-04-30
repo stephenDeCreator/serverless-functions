5a58c50663be169a4e18f002e24f1aaf31efe220
Stephen Yeboah <stephenyeb52@gmail.com>
2021-04-30 07:08:22 +0000

    example 3 complete

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -1,1 +1,24 @@
-const result = document.querySelector('.result')
+const result = document.querySelector('.result');
+
+const fetchPrdoducts = async () => {
+	try {
+		// const { data } = await axios.get('/api/airtable');
+		const { data } = await axios.get('/api/complete');
+		const products = data
+			.map((product) => {
+				const { id, name, url, price } = product;
+				return `<a href='product.html?id=${id}' class='product'>
+      <img src='${url}' alt='${name}'/>
+      <div class='info'>
+      <h5>${name}</h5>
+      <h5 class='price'>$${price}</h5>
+      </div>
+      </a>`;
+			})
+			.join('');
+		result.innerHTML = products;
+	} catch (error) {
+		result.innerHTML = '<h4>There was an error</h4>';
+	}
+};
+fetchPrdoducts();
